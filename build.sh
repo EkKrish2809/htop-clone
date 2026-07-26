5c9405f5e17f7af7ff4856aa64650f4cfd4edc80
Kishor Ekbote <ekbotekishor115@gmail.com>
2026-07-26 23:56:19 +0530

    reading /proc/stat and be able to print it. Content is not perfectly organizable to be able to calculate CPU usage. This is next step

---
+++ build.sh
@@ -0,0 +1,5 @@
+rm mtop.out
+
+g++ -g -o mtop.out main.cpp
+
+./mtop.out
--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <unistd.h> // for sysconf
-#include <stdlib.h>
+#include <stdlib.h> // for exit()
+// #include <sys/types.h>
+// #include <sys/stat.h>
+#include <fcntl.h> // for open()
+
+void readCpuStats() {
+    // open the /proc/stat file in read-only mode
+    int stat_fd = 0;
+    stat_fd = open("/proc/stat", O_RDONLY);
+    if (stat_fd < 0) {
+        printf("can't open /proc/stat\n");
+        exit(0);
+    }
+
+    off_t offset = lseek(stat_fd, 0, SEEK_CUR);
+    if (offset < 0) {
+        printf("can't seek to the end of /proc/stat\n");
+        exit(0);
+    } else {
+        printf("offset : %ld\n", offset);
+    }
+    // read 1st line of /proc/stat 
+    char stat_buf[1024 * 2];
+
+    ssize_t bytes_read = read(stat_fd, stat_buf, sizeof(stat_buf));
+    if (bytes_read < 0) {
+        printf("can't read /proc/stat\n");
+        exit(0);
+    } else {
+        printf("cpu stats : %s\nbytes read : %ld\n", stat_buf, bytes_read);
+    }
+    // close the file
+    if (close(stat_fd) < 0) {
+        printf("can't close the file descriptor\n");
+        exit(0);
+    }
+}
 
 int main(){
 
@@ -26,6 +62,7 @@ int main(){
     sleep for 1000ms and read it again
     
     */
+   readCpuStats();
 
     return 0;
 }
